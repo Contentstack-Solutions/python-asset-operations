94ca3c8d1025512a3526c579aa364f01d2c930b9
Vidar Masson <vidar.masson@contentstack.com>
2023-05-26 09:41:36 +0000

    Mostly working with folder upload

--- uploadAssetsWithStructure.py
+++ uploadAssetsWithStructure.py
@@ -2,6 +2,10 @@
 Bulk create files and folders
 vidar.masson@contentstack.com
 2023-05-25
+
+Caveats:
+- This script does not handle duplicate filenames, it will happily upload two files with the same name to the same folder.
+
 '''
 import os
 import cma
@@ -24,8 +28,8 @@ def getMetadata(parentFolderUid, filename):
         }
 
 
-file_list = []
-folderlookup = {}
+file_list = [] # file_list contains all files paths that should be synced from local filesystem to the stack
+folderlookup = {}  # folderlookup contains data on all folders in the stack
 
 all_folders = cma.getAllFolders()['assets']
 for folder in all_folders:
@@ -49,9 +53,13 @@ for folder in folderlookup.keys():
     folderlookup[folder]['path'] = '/'.join(path)
     path2uid[folderlookup[folder]['path']] = folderlookup[folder]['uid']
 
-pp(path2uid)
-pp(folderlookup)
+#pp(path2uid)
+#pp(folderlookup)
 
+
+'''
+Start by creating all folders and make a list of files to by synced
+'''
 for root, dirs, files in os.walk(rootfolder):
     for name in files:
         full = os.path.join(root, name)
@@ -63,14 +71,20 @@ for root, dirs, files in os.walk(rootfolder):
         relative = os.path.relpath(full, rootfolder)
         if relative not in path2uid:
             # create folder
-            print('Creating folder: ' + relative)
-            parent = os.path.dirname(relative)
-            newFolder = cma.createFolder(name, )
-            folderlookup[newFolder['folder']['uid']] = {
-                'uid': newFolder['folder']['uid'],
-                'parent_uid': newFolder['folder']['parent_uid'],
-                'name': newFolder['folder']['name'],
+            parent_path = os.path.dirname(relative)
+            newFolder = cma.createFolder(name, path2uid[parent_path] if parent_path in path2uid else None)
+            # Update folderlookup base storage
+            folderlookup[newFolder['asset']['uid']] = {
+                'uid': newFolder['asset']['uid'],
+                'parent_uid': newFolder['asset']['parent_uid'],
+                'name': newFolder['asset']['name'],
                 'path': relative,
             }
+            # Update path2uid lookups
+            path2uid[relative] = newFolder['asset']['uid']
             
-        
+
+for item in file_list:
+    parent_uid = path2uid[os.path.dirname(item)] if os.path.dirname(item) in path2uid else None
+    fname = os.path.basename(item)
+    cma.createAsset(rootfolder + item, getMetadata(parent_uid, fname), fname)
